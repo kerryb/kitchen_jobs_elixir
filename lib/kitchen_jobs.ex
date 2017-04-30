7f8f58c3530a2ffb3b544b11b874634980bf59dc
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-30 11:13:13 +0100

    Skeleton implementation

--- lib/kitchen_jobs.ex
+++ lib/kitchen_jobs.ex
@@ -1,5 +1,15 @@
 defmodule KitchenJobs do
+  alias KitchenJobs.Roster
+
   def main input do
-    ""
+    input
+    |> parse
+    |> Enum.map(&Roster.parse/1)
+    |> Enum.map(&Roster.output/1)
+    |> Enum.join
+  end
+
+  def parse(input) do
+    []
   end
 end
---
+++ lib/kitchen_jobs/roster.ex
@@ -0,0 +1,8 @@
+defmodule KitchenJobs.Roster do
+  def parse(input) do
+  end
+
+  def output(roster) do
+    ""
+  end
+end
